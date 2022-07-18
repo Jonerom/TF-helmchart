4cf57f1f0c3a06e87fc0b5344b18c4ece6492e31
Jonerom <xowd69@gmail.com>
2022-07-18 22:42:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,15 @@
-resource "google_container_cluster" "primary" {
-  name                     = var.name
-  location                 = var.region
-  remove_default_node_pool = true
-  initial_node_count       = var.initial_node_count
-  min_master_version       = "1.20"
-}
+resource "helm_release" "bitnami_wordpress" {
+  name       = var.wp_name
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "wordpress"
 
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "${var.name}-node-pool"
-  cluster    = var.name
-  location   = var.region
-  node_count = var.initial_node_count
+  set {
+    name  = "wordpressUsername"
+    value = var.wp_username
+  }
 
-  node_config {
-    preemptible  = false
-    machine_type = var.machine_type
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+  set {
+    name  = "wordpressPassword"
+    value = var.wp_password
   }
 }
