2a8f2da600470a4cbe5d45380689ed3ae3409ae1
Jonerom <xowd69@gmail.com>
2022-07-18 22:42:38 +0200

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  credentials = file(var.credentials)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${data.google_container_cluster.primary.endpoint}"
+    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+    token                  = data.google_client_config.current.access_token
+  }
+}
